d1e00222d3f17e1aa2ea75cabab0f387bb76ac52
showa_yojyo <yojyo@hotmail.com>
2020-07-02 21:47:58 +0900

    (IPython) Suppress ConnectionResetError

---
+++ .ipython/profile_default/startup/disable.py
@@ -0,0 +1,9 @@
+# dummy script in order to suppress ConnectionResetError
+# from the module asyncio.windows_events.
+
+# import asyncio.windows_events
+# import warnings
+#
+# warnings.filterwarnings(
+#     "default", category=ConnectionResetError,
+#     module=asyncio.windows_events)
