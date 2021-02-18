f03860e3eb3635d4442f38f8ee8681af1ecdf45f
showa_yojyo <yojyo@hotmail.com>
2021-02-18 21:40:50 +0900

    .ipython/profile_pandas: read CSV files

--- .ipython/profile_pandas/startup/init.py
+++ .ipython/profile_pandas/startup/init.py
@@ -1,4 +1,15 @@
+"""
+IPython pandas profile
+"""
+
 import numpy as np
 import pandas as pd
-import sys
+#import matplotlib.pyplot as plt
 
+def load_titanic():
+    """Return an instance of DataFrame from a CSV file"""
+    return pd.read_csv("data/titanic.csv")
+
+def load_air_quality():
+    """Return an instance of DataFrame from a CSV file"""
+    return pd.read_csv("data/air_quality_no2.csv", index_col=0, parse_dates=True)
